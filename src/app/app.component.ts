a186248e8dc1d18cb1363036177a87096421010c
Andrii Kapinos <broxbeats@gmail.com>
2020-12-06 06:10:08 +0200

    added console basis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'switch-console';
+export class AppComponent  {
+  name = 'Angular 5';
+
+  messages = [
+    'Remote:~ user$ authenticate',
+    'Checking stored credentials...',
+    'Authentication successful.'
+  ];
+
+  ngOnInit() {
+    this.setFocus();
+  }
+
+  setFocus() {
+    document.getElementById("text-input")?.focus();
+  }
+
+  submit(text: string) {
+    this.messages.push('Remote:~ user$ ' + text);
+    this.scrollToBottom();
+  }
+
+  scrollToBottom() {
+    setTimeout(
+      () => {
+        window.scrollTo(0,document.body.scrollHeight);
+      },
+      50
+    );
+  }
 }
